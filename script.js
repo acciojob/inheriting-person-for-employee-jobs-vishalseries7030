db97b99d03ce338e113db99b87eda084fba531ed
vishal payyawar <139535850+vishalseries7030@users.noreply.github.com>
2025-08-31 17:51:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ class Person {
   }
 
   greet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
   }
 }
 
 class Employee extends Person {
   constructor(name, age, jobTitle) {
-    super(name, age); // calling parent constructor
+    super(name, age);
     this.jobTitle = jobTitle;
   }
 
